d757aa239393fd434dc85578179c296c0beb0406
Jeffrey Chan <superjeffreyc.cs@gmail.com>
2016-06-23 18:05:46 -0400

    Added Mergesort comments

--- Sorting/Mergesort/Mergesort.cpp
+++ Sorting/Mergesort/Mergesort.cpp
@@ -3,53 +3,69 @@
 
 using namespace std;
 
-int * Mergesort::mergesort(int * a, int size) {
+// Recursively split the array into two parts until there are only 1 or 2 elements remaining, then merge everything back together
+int * Mergesort::mergesort(int * array, int size) {
 
-	if (size == 1) return a;
-	else if (size == 2) {
-		if (a[0] > a[1]) swap(a[0], a[1]);
-		return a;
+	// Base cases for recursive calls
+	if (size == 1) return array;	// A single element is already sorted
+	else if (size == 2) {		// Manually sort two elements
+		if (array[0] > array[1]) swap(array[0], array[1]);
+		return array;
 	}
 
-	int * aLeft = new int[size / 2];
-	int * aRight = new int[size - size / 2];
+	// Create two new arrays for each half
+	int * arrayLeft = new int[size / 2];
+	int * arrayRight = new int[size - size / 2];
 
+	// Fill the first array with the left half
 	for (int i = 0; i < size / 2; i++) {
-		aLeft[i] = a[i];
+		arrayLeft[i] = array[i];
 	}
 
+	// Fill the second array with the right half
+	// Needs two counters: one for half-filled original array and one for arrayRight
 	for (int i = 0, j = size / 2; j < size; i++, j++) {
-		aRight[i] = a[j];
+		arrayRight[i] = array[j];
 	}
 
-	aLeft = mergesort(aLeft, size / 2);
-	aRight = mergesort(aRight, size - size / 2);
-	int * merged = merge(aLeft, aRight, size / 2, size - size / 2);
+	// Recursively split the two arrays in half again
+	arrayLeft = mergesort(arrayLeft, size / 2);
+	arrayRight = mergesort(arrayRight, size - size / 2);
+	
+	// Merge the two halves back together in sorted order
+	int * merged = merge(arrayLeft, arrayRight, size / 2, size - size / 2);
 
+	// Copy the sorted array into the original array
 	for (int i = 0; i < size; i++) {
-		a[i] = merged[i];
+		array[i] = merged[i];
 	}
 
-	return a;
+	return array;
 }
 
-int * Mergesort::merge(int * a, int * b, int sizeA, int sizeB) {
-	int * array = new int[sizeA + sizeB];
-	int i = 0, j = 0, k = 0;
+// Merge two smaller arrays into a single larger array
+int * Mergesort::merge(int * arrayA, int * arrayB, int sizeA, int sizeB) {
+	int * array = new int[sizeA + sizeB];	// Create enough space to combine two arrays
+	int aIndex = 0, bIndex = 0;		// Indices for the two arrays
+	int i = 0;				// Index for the new array
 
-	for (; i < sizeA && j < sizeB; k++) {
-		if (a[i] < b[j]) array[k] = a[i++];
-		else array[k] = b[j++];
+	// Comparing elements starting at the beginning of both array A and B, put the smaller element into the new array
+	// Then, increment the counter for either array A or B, depending on which one inserted a value into the new array
+	for (; aIndex < sizeA && bIndex < sizeB; i++) {
+		if (arrayA[aIndex] < arrayB[bIndex]) array[i] = arrayA[aIndex++];
+		else array[i] = arrayB[bIndex++];
 	}
 
-	if (i < sizeA) {
-		for (; i < sizeA; k++) {
-			array[k] = a[i++];
+	// If array A still has elements
+	if (aIndex < sizeA) {
+		for (; aIndex < sizeA; i++) {
+			array[i] = arrayA[aIndex++];
 		}
 	}
+	// array B still has more elements
 	else {
-		for (; j < sizeB; k++) {
-			array[k] = b[j++];
+		for (; bIndex < sizeB; i++) {
+			array[i] = arrayB[bIndex++];
 		}
 	}
 
